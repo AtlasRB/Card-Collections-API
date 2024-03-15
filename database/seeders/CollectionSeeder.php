a3212e85110db8459efd1a3d93b14d586383dbf9
Connor Yeeles <connoryeeles@outlook.com>
2024-03-15 14:17:10 +0000

    Initial addition of all folders and files to repository

---
+++ database/seeders/CollectionSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CollectionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 151; $i++) {
+            DB::table('cards')->insert([
+                'name' => Str::random(10),
+                'number' => rand(1,151),
+                'type' => Str::random(10),
+                'collected' =>rand(0,1)
+            ]);
+        }
+    }
+}
