6c7e740351c739aebf6458f19a6e69e1f98265c8
Connor Yeeles <connoryeeles@outlook.com>
2024-03-15 14:58:07 +0000

    Added get all / get single / change / add / delete functions into CollectionController

---
+++ app/Http/Controllers/CollectionController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\cards;
+use Illuminate\Http\Request;
+
+class CollectionController extends Controller
+{
+    public function getAllCards()
+    {
+        $cards = cards::all();
+        return response()->json([
+            'message' => 'Presenting all cards',
+            'data' => $cards
+        ]);
+    }
+
+    public function getSingleCard(int $id)
+    {
+        $card = cards::find($id);
+        return response()->json([
+            'message' => 'Presenting single card',
+            'data' => $card
+        ]);
+    }
+
+    public function changeCard(int $id, Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:50|string',
+            'number' => 'required|min:0|integer',
+            'type' => 'required|max:50|string',
+            'collected' => 'required|boolean'
+        ]);
+
+        $card = cards::find($id);
+        $card->name = $request->name;
+        $card->number = $request->number;
+        $card->type = $request->type;
+        $card->collected = $request->collected;
+
+        if (!$card->save()) {
+            return response()->json([
+                'message' => 'Could not update card'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Card updated',
+            'data' => $card
+        ]);
+    }
+
+    public function addCard(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:50|string',
+            'number' => 'required|min:0|integer',
+            'type' => 'required|max:50|string',
+            'collected' => 'required|boolean'
+        ]);
+
+        $card = new cards();
+        $card->name = $request->name;
+        $card->number = $request->number;
+        $card->type = $request->type;
+        $card->collected = $request->collected;
+
+        if (!$card->save()) {
+            return response()->json([
+                'message' => 'Could not add card'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Card added',
+            'data' => $card
+        ], status: 201);
+    }
+
+    public function deleteCard(int $id)
+    {
+        $card = cards::find($id);
+        $card->delete();
+        return response ()->json([
+            'message' => 'Card deleted'
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\CollectionController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route:: controller(CollectionController::class)->group(function() {
+    Route::get('/cards', 'getAllCards');
+    Route::get('/cards/{id}', 'getSingleCard');
+    Route::put('/cards/{id}', 'changeCard');
+    Route::post('/cards', 'addCard');
+    Route::delete('/cards/{id}', 'deleteCard');
+});
